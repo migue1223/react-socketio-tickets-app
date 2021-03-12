994c8a664e6dd00496a6858dbc8fc60bf0600238
desarrollo-opting <contabilidad@opting.co>
2021-03-12 17:20:31 -0500

    Codigo inicial

---
+++ backend/models/sockets.js
@@ -0,0 +1,34 @@
+const TicketList = require('./ticket-list');
+
+class Sockets {
+  constructor(io) {
+    this.io = io;
+
+    this.ticketList = new TicketList();
+
+    this.socketEvents();
+  }
+
+  socketEvents() {
+    // On connection
+    this.io.on('connection', (socket) => {
+      console.log('cliente conectado')
+      // Escuchar evento: mensaje-to-server
+      socket.on('solicitar-ticket', (data, callback) => {
+        const nuevoTicket = this.ticketList.crearTicket();
+        callback(nuevoTicket);
+      });
+
+      socket.on(
+        'siguiente-ticket-trabajar',
+        ({ agente, escritorio }, callback) => {
+          const suTicket = this.ticketList.asignarTicket(agente, escritorio);
+          callback(suTicket);
+          this.io.emit('ticket-asignado', this.ticketList.ultimos13);
+        }
+      );
+    });
+  }
+}
+
+module.exports = Sockets;
